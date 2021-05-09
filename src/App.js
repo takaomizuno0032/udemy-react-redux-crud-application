f7b1537c287f6910c3b1c0e92ef3bf1f241b7df7
水野喬夫 <takao@mizunotakao.local>
2021-05-09 22:08:20 +0900

    proos

--- src/App.js
+++ src/App.js
@@ -15,8 +15,32 @@ import React, { Component } from "react";
 //   }
 // }
 
-const App = ()=> {
-  return <div>Hi</div>
+const App = () => {
+  const profiles = [
+    {
+      name: "Taro", age: 10
+    },
+    {
+      name: "Hanako", age: 5
+    }
+  ]
+  return(
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index}/>
+
+        })
+      }
+    </div>
+
+  ) 
+}
+
+const User = (props) => {
+  return (
+      <div>Hi, I am {props.name} and {props.age} years old.</div>
+    )
 }
 
 export default App;
