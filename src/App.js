42a92e68cacd444b3c2438ce9a82158ade5699b9
水野喬夫 <takao@mizunotakao.local>
2021-05-09 21:48:04 +0900

    componets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 
-class App extends Component{
-  render(){
-    // const greeting = "Hi!";
+// class App extends Component{
+//   render(){
+//     // const greeting = "Hi!";
     
-    // const dom = <input type="text" onChange={ () => {console.log("Hello")}}/>
-    // return dom;
-    return(
-      <div>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={ () => {console.log("Hello")}}/>
-      </div>
-    )
-  }
+//     // const dom = <input type="text" onChange={ () => {console.log("Hello")}}/>
+//     // return dom;
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={ () => {console.log("Hello")}}/>
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = ()=> {
+  return <div>Hi</div>
 }
 
 export default App;
